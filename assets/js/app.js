8bc39a9c406093a4cd2b4ac465896b9e275a94d7
Jonas Hoppe <j.hoppe@nexus-united.com>
2020-03-22 16:36:11 +0100

    somebody keeps erasing my changes.

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+import 'startbootstrap-sb-admin-2/js/sb-admin-2'
+import 'bootstrap'
+import 'jquery.easing/jquery.easing.min'
